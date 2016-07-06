63e7cd0f41eca294c1247789e4d67d98e02443d0
Criibyy <drivegirl_schatje@hotmail.com>
2016-07-06 16:37:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,20 +4,16 @@ var fs = require("fs"),
     IntervalInt = null,
     readlineSync = require("readline-sync"),
     Protos = require("./protos/protos.js"),
-    CountReports = 0,
+    CountCommends = 0,
     Long = require("long"),
-    SteamClients = {},
-    SteamUsers = {},
-    SteamGCs = {},
-    SteamFriends = {},
-    process = require("process");
-    steamID = readlineSync.question("SteamID64 which will be reported: ");
-
+    process = require("process"),
+    steamID = readlineSync.question("SteamID64 which will be commended: ");
+ 
 var ClientHello = 4006,
     ClientWelcome = 4004;
-
+ 
 var accounts = [];
-
+ 
 var arrayAccountsTxt = fs.readFileSync("accounts.txt").toString().split("\n");
 for (i in arrayAccountsTxt) {
     var accInfo = arrayAccountsTxt[i].toString().trim().split(":");
@@ -29,106 +25,133 @@ for (i in arrayAccountsTxt) {
         password: password
     });
 }
-
-arrayAccountsTxt.forEach(processSteamReport);
-
-function processSteamReport(element, indexElement, array) {
-    if(element != "") {
-        var account = element.toString().trim().split(":");
-        var account_name = account[0];
-        var password = account[1];
-        SteamClients[indexElement] = new Steam.SteamClient();
-        SteamUsers[indexElement] = new Steam.SteamUser(SteamClients[indexElement]);
-        SteamGCs[indexElement] = new Steam.SteamGameCoordinator(SteamClients[indexElement], 730);
-        SteamFriends[indexElement] = new Steam.SteamFriends(SteamClients[indexElement]);
-        
-        SteamClients[indexElement].connect();
-
-        SteamClients[indexElement].on("connected", function() {
-            SteamUsers[indexElement].logOn({
+ 
+function loginAndCommend(steamID) {
+    if ((steamID == "") || !(steamID.indexOf("765") > -1) || (steamID.length < 17)) {
+        console.log("That's not a valid SteamID!");
+        process.exit();
+    }
+    if (accounts[0]) {
+        var account = accounts[0][0];
+        var account_name = account.username;
+        var password = account.password;
+        Client = new Steam.SteamClient();
+        User = new Steam.SteamUser(Client);
+        GC = new Steam.SteamGameCoordinator(Client, 730);
+        Friends = new Steam.SteamFriends(Client);
+ 
+        Client.connect();
+ 
+        Client.on("connected", function() {
+            User.logOn({
                 account_name: account_name,
                 password: password
             });
         });
-
-        SteamClients[indexElement].on("logOnResponse", function(res) {
+ 
+        Client.on("logOnResponse", function(res) {
             if (res.eresult !== Steam.EResult.OK) {
                 if (res.eresult == Steam.EResult.ServiceUnavailable) {
                     console.log("\n[STEAM CLIENT - " + account_name + "] Login failed - STEAM IS DOWN!");
                     console.log(res);
-                    SteamClients[indexElement].disconnect();
+                    Client.disconnect();
                     process.exit();
                 } else {
                     console.log("\n[STEAM CLIENT - " + account_name + "] Login failed!");
                     console.log(res);
-                    SteamClients[indexElement].disconnect();
+                    Client.disconnect();
+                    accounts.splice(0, 1);
+                    loginAndCommend(steamID);
                 }
             } else {
-                SteamFriends[indexElement].setPersonaState(Steam.EPersonaState.Offline);
-
-                SteamUsers[indexElement].gamesPlayed({
+                console.log("\n[STEAM CLIENT - " + account_name + "] Logged in!");
+ 
+                Friends.setPersonaState(Steam.EPersonaState.Offline);
+ 
+                User.gamesPlayed({
                     games_played: [{
                         game_id: 730
                     }]
                 });
-
-                if (SteamGCs[indexElement]) {
+ 
+                if (GC) {
                     IntervalInt = setInterval(function() {
-                        SteamGCs[indexElement].send({
+                        GC.send({
                             msg: ClientHello,
                             proto: {}
                         }, new Protos.CMsgClientHello({}).toBuffer());
                     }, 2000);
+                    console.log("[GC - " + account_name + "] Client Hello sent!");
                 } else {
-                    SteamClients[indexElement].disconnect();
+                    console.log("[GC - " + account_name + "] Not initialized!");
+                    Client.disconnect();
+                    accounts.splice(0, 1);
+                    loginAndCommend(steamID);
                 }
             }
         });
-
-        SteamClients[indexElement].on("error", function(err) {
-            console.log("[STEAM CLIENT - " + account_name + "] Account is probably ingame! Logged out!");
-            SteamClients[indexElement].disconnect();
+ 
+        Client.on("error", function(err) {
+            console.log("[STEAM CLIENT - " + account_name + "] " + err);
+            console.log("[STEAM CLIENT - " + account_name + "] Account is probably ingame!");
+            Client.disconnect();
+            accounts.splice(0, 1);
+            loginAndCommend(steamID);
         });
-
-        SteamGCs[indexElement].on("message", function(header, buffer, callback) {
+ 
+        GC.on("message", function(header, buffer, callback) {
             switch (header.msg) {
                 case ClientWelcome:
                     clearInterval(IntervalInt);
-                    sendReport(SteamGCs[indexElement], SteamClients[indexElement], account_name, steamID);
+                    console.log("[GC - " + account_name + "] Client Welcome received!");
+                    console.log("[GC - " + account_name + "] Commend request sent!");
+                    sendCommend(GC, Client, account_name, steamID);
                     break;
                 case Protos.ECsgoGCMsg.k_EMsgGCCStrike15_v2_MatchmakingGC2ClientHello:
-                    break;
-                case Protos.ECsgoGCMsg.k_EMsgGCCStrike15_v2_ClientReportResponse:
-                    CountReports++;
-                    console.log("[GC - " + account_name + "] (" + CountReports + ") Report with confirmation ID: " + Protos.CMsgGCCStrike15_v2_ClientReportResponse.decode(buffer).confirmationId.toString() + " sent!");
-                    SteamClients[indexElement].disconnect();
+                    console.log("[GC - " + account_name + "] MM Client Hello sent!");
                     break;
                 default:
                     console.log(header);
                     break;
             }
         });
+    } else {
+        console.log("\n\n" + CountCommends + " commend(s) successfully sent!");
+        Client.disconnect();
     }
 }
-
-function sendReport(GC, Client, account_name) {
+ 
+function sendCommend(GC, Client, account_name) {
+    console.log("[GC - " + account_name + "] Commend request received!");
+    console.log("[GC - " + account_name + "] Trying to commend the user!");
     var account_id = new SteamID(steamID).accountid;
-    GC.send({
-        msg: Protos.ECsgoGCMsg.k_EMsgGCCStrike15_v2_ClientReportPlayer,
-        proto: {}
-    }, new Protos.CMsgGCCStrike15_v2_ClientReportPlayer({
+	
+	var commend_payload = new Protos.PlayerCommendationInfo({
+		cmdFriendly: 1,
+		cmdTeaching: 2,
+		cmdLeader: 4
+	});
+	
+	var commendProto = new Protos.CMsgGCCStrike15_v2_ClientCommendPlayer({
         accountId: account_id,
         matchId: 8,
-        rptAimbot: 2,
-        rptWallhack: 3,
-        rptSpeedhack: 4,
-        rptTeamharm: 5,
-        rptTextabuse: 6,
-        rptVoiceabuse: 7
-    }).toBuffer());
+		tokens: 10,
+		commendation: commend_payload
+    }).toBuffer();
+	
+    GC.send({
+        msg: Protos.ECsgoGCMsg.k_EMsgGCCStrike15_v2_ClientCommendPlayer,
+        proto: {}
+    }, commendProto);
+	console.log("[GC - " + account_name + "] Commendation Sent!");
+	Client.disconnect();
+    accounts.splice(0, 1);
+    CountCommends++;
+    loginAndCommend(steamID);
 }
-
+ 
 process.on('uncaughtException', function (err) {
 });
-
-console.log("Initializing ReportBot by askwrite...\nCredits: Trololo - Idea");
+ 
+loginAndCommend(steamID);
+console.log("Initializing CommendBot by CoolA1d...\nCredits: AskWrite for original ReportBot, Trololo - Idea for ReportBot");
